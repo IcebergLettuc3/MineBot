bb9909c923412b3b3e3ce52fa42efab51db7b9ae
Jonathan Eisenbrandt <jeisenbrandt1@gmail.com>
2023-11-30 23:36:44 -0600

    unsuccessful

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from time import sleep
 import subprocess
 import os
 from dotenv import load_dotenv
+import matplotlib.pyplot as plt
+from PIL import Image
+import cv2
 
 def nav_to_image(image, clicks, off_x=0, off_y=0):
   position = pt.locateCenterOnScreen(image, confidence=0.9)
@@ -40,20 +43,31 @@ def locate_lava():
 # sleep(10)
 # nav_to_image('images/start_game.png', 3)
 
+print("Working dir:", os.getcwd())
+print("Files in here:", os.listdir("."))
+
 #open minecraft launcher
 load_dotenv()
 minecraft_launcher_path = os.getenv('MINECRAFTLAUNCHERPATH')
 print("path: ",minecraft_launcher_path)
 subprocess.Popen(minecraft_launcher_path)
 #wait for launcher to finish loading
-sleep(2)
+# sleep(2)
+image_path = 'MineBot\ play.png'
+img = Image.open(image_path)
+img2 = cv2.imread(image_path)
+plt.imshow(img)
+plt.axis('off')  # Turn off axis numbers
+# plt.show()
 #start minecraft
 try:
+  # print("test")
   # Locate the center of the image on screen
-  location = pt.locateCenterOnScreen('MineBot\images\play.png', confidence=0.7)
+  location = pt.locateCenterOnScreen(img2, confidence=0.7)
+  print("location: ", location)
     
   if location is not None:
-        # Move to the location and click
+    print("image found")
     pt.moveTo(location, duration=0.1)
     pt.click()
   else:
